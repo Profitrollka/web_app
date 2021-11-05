76ef4c0cfd3b354ac9ccc70f5149f9808b97e8a7
Alena Kapustyan <alenka-1101@ya.ru>
2021-11-05 22:45:57 +0300

    Updated project files

--- config.py
+++ config.py
@@ -5,18 +5,18 @@ from dotenv import load_dotenv
 basedir = path.abspath(path.dirname(__file__))
 load_dotenv(path.join(basedir, '.env'))
 
-class Configuration(object):
+
+class BaseConfig:
     """Set Flask configuration from .env file."""
 
-    DEBUG = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Database
     SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_MIGRATE_REPO = path.join(basedir, 'db_repository')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
     # SQLALCHEMY_ECHO = False
 
     # General Config
-    # SECRET_KEY = environ.get('SECRET_KEY')
-    # FLASK_APP = environ.get('FLASK_APP')
-    # FLASK_ENV = environ.get('FLASK_ENV')
+    SECRET_KEY = environ.get('SECRET_KEY')
+    FLASK_APP = environ.get('FLASK_APP')
+    FLASK_ENV = environ.get('FLASK_ENV')
--- main.py
+++
@@ -1,7 +0,0 @@
-from app import app, db
-import view
-import models
-
-
-if __name__ == '__main__':
-    app.run()
